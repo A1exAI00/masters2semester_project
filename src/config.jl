5cd0f9f5b6da44f461a84e8f4b0485051ddd9863
Alex Akinin <lksndrakinin@gmail.com>
2025-03-28 15:33:42 +0300

    Add general config file

---
+++ src/config.jl
@@ -0,0 +1,7 @@
+using DrWatson
+@quickactivate "masters2semester_project"
+
+# Directory in "plots" to save plots to
+GENERAL_PLOT_SAVE_DIR = "tmp01"
+GENERAL_PLOT_SAVE_PATH = plotsdir(GENERAL_PLOT_SAVE_DIR)
+
